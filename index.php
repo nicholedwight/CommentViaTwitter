4db6cd38987d5f8f5749dd50e42be494f5d355c7
Nichole Dwight <nicholedwight@gmail.com>
2015-01-10 11:16:00 +0000

    twitter authentication works and so does redirect to index

--- index.php
+++ index.php
@@ -1,10 +1,4 @@
-<?php
 
-require_once( './dbconnect.php');
-$db = connectToDatabase();
-?>
 
-<form>
-  <textarea name="comment" id="comment" rows="6" cols="40" placeholder="Comment:" required></textarea>
-  <button class="submit" type="submit">Submit</button>
-</form>
+<h1>Hello</h1>
+<!--TwitterOauth from https://github.com/abraham/twitteroauth -->
