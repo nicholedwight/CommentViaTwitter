7c03861f321ded3964f5b167050c867ad29b86e6
Nichole Dwight <nicholedwight@gmail.com>
2015-01-25 12:14:26 +0000

    added a back button on battle info pages

--- battle.php
+++ battle.php
@@ -2,7 +2,9 @@
 include('inc/header.php');
 include('inc/lib.php');
 $battle_id = $_GET['id'];
-echo $battle_id;
+?>
+<a href="index.php">Back</a>
+<?php
 include('inc/comment_form.php');
 include('inc/footer.php');
 ?>
