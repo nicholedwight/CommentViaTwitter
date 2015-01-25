1901663b25892ca6194180d31694cf6c68506e87
Nichole Dwight <nicholedwight@gmail.com>
2015-01-25 13:47:15 +0000

    styling comments area, adding classes and wrappers so i can migrate it into the civil war app seamlessly

--- battle.php
+++ battle.php
@@ -3,7 +3,7 @@ include('inc/header.php');
 include('inc/lib.php');
 $battle_id = $_GET['id'];
 ?>
-<a href="index.php">Back</a>
+<a href="index.php">Back to battles</a>
 <?php
 include('inc/comment_form.php');
 include('inc/footer.php');
--- inc/comment_form.php
+++ inc/comment_form.php
@@ -21,40 +21,55 @@ if($_SERVER['REQUEST_METHOD']=="POST"){
 
 //Pulling all comments from db by battle id
 ?>
-<ul class="comment_list">
-<?php
-$commentRows = getAllCommentsByBattleID($battle_id);
-if ($commentRows) { //If there any comments for this page, display them
-  foreach ($commentRows as $comment) {
-    $date = date('F j, Y, g:i a', strtotime($comment['created_at']));
-    echo '<li>';
-    echo '<img src="' . $comment['profile_image_url'] . '"> ' . $comment['username'] . '<br>';
-    echo $comment['comment'] . '<br>';
-    echo $date . '</li>';
-  }
-} else {
-  echo "No comments have been left yet!";
-}
-?>
-</ul>
-
-<h1>
-  <?=(!empty($_SESSION['access_token']['screen_name'])
-              ? '@' . $_SESSION['access_token']['screen_name'] .
-              " <img src='" . $_SESSION['profile_image_url'] . "'>"
-              : 'Guest'); ?>
-</h1>
-
-<?php
-if (!$_SESSION) {
-  echo "<a href='redirect.php'>Login</a>";
-} else { ?>
-  <form method="POST" action="" class="comment_form">
-    <textarea name="comment" id="comment" rows="6" cols="40" placeholder="Comment:" required></textarea>
-    <input type="hidden" value="<?php echo $_GET['id'];?>" name="battle_id">
-    <button class="submit" type="submit">Submit</button>
-  </form>
+<section class="comment_section_wrapper">
+  <ul class="comment_list">
   <?php
-  echo "<a href='logout.php'>Logout</a>";
-}
-?>
+  $commentRows = getAllCommentsByBattleID($battle_id);
+  if ($commentRows) { //If there any comments for this page, display them
+    foreach ($commentRows as $comment) {
+      $date = date('F j, Y, g:i a', strtotime($comment['created_at'])); ?>
+        <li>
+          <img src="<?php echo $comment['profile_image_url'];?>" alt="" class="avatar">
+          <!--Empty alt tag because it provides no important info for users via screenreader-->
+          <div class="comment_content_wrapper">
+             <div class="comment_user_info">
+               <p>
+                 <strong><?php echo $comment['username'];?></strong>
+                 <span class="comment_date"><?php echo $date;?></span>
+               </p>
+             </div>
+             <div class="comment_content">
+               <?php echo $comment['comment'];?>
+             </div>
+          </div>
+        </li>
+    <?php
+    }
+  } else {
+    echo "No comments have been left yet!";
+  }
+  ?>
+  </ul>
+  <div class="form_wrapper">
+    <h1>
+      <?=(!empty($_SESSION['access_token']['screen_name'])
+                  ? '@' . $_SESSION['access_token']['screen_name'] .
+                  " <img src='" . $_SESSION['profile_image_url'] . "'>"
+                  : 'Guest'); ?>
+    </h1>
+
+    <?php
+    if (!$_SESSION) {
+      echo "<a href='redirect.php'>Login</a>";
+    } else { ?>
+      <form method="POST" action="" class="comment_form">
+        <textarea name="comment" id="comment" rows="6" cols="40" placeholder="Comment:" required></textarea>
+        <input type="hidden" value="<?php echo $_GET['id'];?>" name="battle_id">
+        <button class="submit" type="submit">Submit</button>
+      </form>
+      <?php
+      echo "<a href='logout.php'>Logout</a>";
+    }
+    ?>
+  </div>
+</section>
